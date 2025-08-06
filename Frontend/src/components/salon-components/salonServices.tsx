dc893c5ccb0be66da60137d2773eb9795ae57d16
Anroux <anrouxb314@gmail.com>
2025-08-06 07:55:32 +0200

    Fixed build error

--- Frontend/src/components/salon-components/salonServices.tsx
+++ Frontend/src/components/salon-components/salonServices.tsx
@@ -4,9 +4,6 @@ import { useEffect } from "react";
 import { Table } from "antd/es";
 import type { ColumnsType } from "antd/es/table";
 import { useStyles } from "../../app/salon/bookings/style/styles";
-import {
-  useBookingActions,
-} from "@/providers/booking-provider";
 import "@ant-design/v5-patch-for-react-19";
 import { ISalonService } from "@/providers/salonService-provider/context";
 import { useSalonServiceActions, useSalonServiceState } from "@/providers/salonService-provider";
@@ -17,7 +14,6 @@ const SalonServices = ({}: { salonServices?: ISalonService[] }) => {
   const { styles } = useStyles();
   const { salonServices } = useSalonServiceState();
   // const salonServices = passedSalonServices ?? contextSalonServices;
-  const { getBookingList } = useBookingActions();
   // const { getEmployeeTechnicianList } = useEmployeeTechnicianActions();
   const { getSalonServiceList } = useSalonServiceActions();
 
